949abb32d471537fb89f0963fa9e1b46d58f66d7
DR <dharini.ramakrishnan@gmail.com>
2011-03-08 10:51:12 -0800

    Removed default index.html

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,14 +6,7 @@ describe PagesController do
     it "should be successful" do
       get 'home'
       response.should be_success
-    end
-    
-  it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
-  end
-    
+    end 
   end
 
   describe "GET 'contact'" do
@@ -21,13 +14,6 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    
-  it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
-  end
-  
   end
   
   describe "GET 'about'" do
@@ -35,13 +21,16 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-    
-  it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
-  end
+
   
   end  
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end  
+  end   
+
 
 end
